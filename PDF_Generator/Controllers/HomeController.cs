7e4813ee9f46203e23e0d04573b0782079545215
Wesley Gomes Queiroz <wgomesq@santillana.local>
2022-06-10 19:51:33 -0300

    Add project files.

---
+++ PDF_Generator/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿//https://code-maze.com/create-pdf-dotnetcore/
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using PDF_Generator.Utility;
+using System.IO;
+
+namespace PDF_Generator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private IConverter _converter;
+        public HomeController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        [HttpGet]
+        [Route("criar-pdf")]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                Out = @"C:\Lab\PDF_Generator\PDFCreator\Employee_Report.pdf"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetHTMLString(),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            _converter.Convert(pdf);
+            return Ok("Successfully created PDF document.");
+        }
+    }
+}
